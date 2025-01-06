3aa162f13a802ef7199c8d6e40704c02ddc6e40d
Rex <htetnaing0814@gmail.com>
2025-01-06 23:46:23 +0700

    row on laptop; col on mobile

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -5,7 +5,7 @@ import { AnalogClock, MarkerPopup } from "../components";
 const Root: React.FC = () => {
   return (
     <PageLayout>
-      <div className="w-48 flex flex-col justify-center items-center">
+      <div className="w-full flex flex-col md:flex-row justify-center md:gap-48 items-center">
         <AnalogClock />
         <MarkerPopup />
       </div>
