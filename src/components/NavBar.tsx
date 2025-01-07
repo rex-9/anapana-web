b16cffeba7b912ce554c445cd648717156182742
Rex <htetnaing0814@gmail.com>
2025-01-07 11:58:00 +0700

    add title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ThemeToggle from "./ThemeToggle";
 import LanguageSwitcher from "./LanguageSwitcher";
 import ProfileAvatar from "./ProfileAvatar";
+import { AppLocales } from "../locales/app_locales";
+import { useTranslation } from "react-i18next";
 
 const NavBar: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <nav className="flex gap-2 justify-between items-center w-full p-2">
-      <div>
+      <div className="w-48">
         <ProfileAvatar className="ml-auto" />
       </div>
+      {t(AppLocales.AnapanaTimerTitle)}
       <div className="flex gap-2 items-center px-2">
         <ThemeToggle />
         <LanguageSwitcher />
